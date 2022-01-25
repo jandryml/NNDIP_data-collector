81325666666c3d09497b5a7a2d904045a4917b72
Jan Dryml <honza.dryml@icloud.com>
2022-01-25 13:51:24 +0100

    data collector - update

--- data-collector/src/main/java/cz/edu/upce/fei/datacollector/tasks/ConnectionHandlerTask.java
+++ data-collector/src/main/java/cz/edu/upce/fei/datacollector/tasks/ConnectionHandlerTask.java
@@ -8,52 +8,35 @@ import java.util.*;
 
 public class ConnectionHandlerTask implements Runnable {
     private static final int THREAD_SLEEP = 15 * 1000;
-    private static final Map<String, SerialPort> handledPorts = new HashMap<>();
+    private static final Set<String> handledPorts = new HashSet<>();
 
     @Override
-    public void run()  {
+    public void run() {
         try {
             while (true) {
-                List<SerialPort> actualPorts = Arrays.asList(SerialPort.getCommPorts());
-
                 System.out.println("Searching for new connection!");
 
+                SerialPort[] actualPorts = SerialPort.getCommPorts();
+
                 for (SerialPort port : actualPorts) {
-                    if (handledPorts.containsKey(port.getDescriptivePortName())
-                            || port.getDescriptivePortName().equals("User-Specified Port")) {
+                    //TODO make "USB" configurable?
+                    if (handledPorts.contains(port.getPortLocation())
+                            || !port.getDescriptivePortName().contains("USB")) {
                         continue;
                     }
-                    System.out.println("New connection! Connecting on: " + port.getDescriptivePortName());
+
+                    System.out.println("New connection! Connecting on: " + port.getPortLocation());
 
                     port.openPort();
                     registerPortListener(port);
-
-                    handledPorts.put(port.getDescriptivePortName(), port);
-                }
-
-                // removal/close of disconnected listeners
-                // (needed to correctly terminate handling thread)
-                if (!handledPorts.isEmpty()) {
-                    System.out.println("Checking disconnected ports.");
-                    disconnectedPortRemoval(actualPorts);
+                    handledPorts.add(port.getPortLocation());
                 }
 
                 Thread.sleep(THREAD_SLEEP);
             }
         } catch (InterruptedException e) {
+            System.out.println("Interup");
             e.printStackTrace();
-        }
-    }
-
-    private static void disconnectedPortRemoval(List<SerialPort> actualPorts) {
-        Iterator<SerialPort> iterator = handledPorts.values().iterator();
-        while (iterator.hasNext()) {
-            SerialPort port = iterator.next();
-            if (actualPorts.stream().noneMatch(it -> it.getDescriptivePortName().equals(port.getDescriptivePortName()))) {
-                System.out.println("Removing port on: " + port.getDescriptivePortName());
-                port.closePort();
-                iterator.remove();
-            }
         }
     }
 
@@ -61,15 +44,21 @@ public class ConnectionHandlerTask implements Runnable {
         port.addDataListener(new SerialPortDataListener() {
             @Override
             public int getListeningEvents() {
-                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE | SerialPort.LISTENING_EVENT_PORT_DISCONNECTED;
             }
 
             @Override
             public void serialEvent(SerialPortEvent event) {
-                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
-                    return;
-                byte[] newData = new byte[port.bytesAvailable()];
-                System.out.print("Read: " + new String(newData).replace("\0", ""));
+                if (event.getEventType() == SerialPort.LISTENING_EVENT_DATA_AVAILABLE) {
+                    byte[] newData = new byte[port.bytesAvailable()];
+                    port.readBytes(newData, newData.length);
+                    //TODO work with data properly
+                    System.out.print("Read: " + new String(newData).replace("\0", ""));
+                } else if (event.getEventType() == SerialPort.LISTENING_EVENT_PORT_DISCONNECTED) {
+                    System.out.println("Disconnected port: " + port.getPortLocation());
+                    handledPorts.remove(port.getPortLocation());
+                    port.closePort();
+                }
             }
         });
     }
