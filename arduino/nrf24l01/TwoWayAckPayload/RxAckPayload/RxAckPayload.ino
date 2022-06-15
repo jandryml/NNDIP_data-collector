3ee5acc12092dda565987050982dfe7941ec9f0c
Jan Dryml <honza.dryml@icloud.com>
2022-06-16 00:38:30 +0200

    nrf24l01 - real data sending

--- arduino/nrf24l01/TwoWayAckPayload/RxAckPayload/RxAckPayload.ino
+++ arduino/nrf24l01/TwoWayAckPayload/RxAckPayload/RxAckPayload.ino
@@ -1,11 +1,11 @@
 // SimpleRxAckPayload- the slave or the receiver
 
 // knihovny pro nrf24l01
-//==============
+//================
 #include <RF24.h>
 #include <nRF24L01.h>
 #include <SPI.h>
-//==============
+//================
 #include "DHT.h" // knihovna pro AM2120 - teplomer a vlhkomer
 #include "MHZ19.h" // knihovna pro detektor CO2 MH-Z19
 
@@ -23,7 +23,7 @@
 #define deviceID 1 // mente dle potreby, je pouzivano jako id zarizeni
 
 // nastaveni komunikace
-//==============
+//================
 RF24 radio(CE_PIN, CSN_PIN); // nRF24L01 (CE,CSN)
 
 const byte address[6] = {'0','0','0','0','1'};   // Address of this node
@@ -39,7 +39,7 @@ struct {
 } dataStruct;
 
 // nastaveni sensoru
-//==============
+//================
 // vytvoření instance senzoru z knihovny,
 // s nastavením typu DHT22, který má stejný
 // typ komunikace jako AM2120
@@ -47,20 +47,16 @@ DHT dht(pinDHT, DHT22);
 // vytvoření objektů z knihovny
 MHZ19 *mhz19_uart = new MHZ19(rx_pin, tx_pin);
 MHZ19 *mhz19_pwm = new MHZ19(pwmpin);
-//==============
+//================
 
 void setup() {
-    dataStruct.teplota = 20.1f;
-    dataStruct.vlhkost = 25.5f;
-    dataStruct.co2ppm = 400;
-    dataStruct.co2ppm2 = 420;
-    dataStruct.teplota2 = 15;
+    cleanData();
 
     Serial.begin(9600);
     Serial.println("SimpleRxAckPayload Starting");
 
     // sensor setup
-    //==============
+    //================
     // zahájení komunikace se senzorem AM2120
     dht.begin();
     // zahájení komunikace se senzorem MH-Z19 přes UART
@@ -69,7 +65,7 @@ void setup() {
     mhz19_uart->setAutoCalibration(false);
 
     // communication setup
-    //==============
+    //================
     radio.begin();
     radio.setDataRate( RF24_2MBPS );
     radio.setChannel(125);
@@ -80,13 +76,16 @@ void setup() {
     radio.writeAckPayload(1, &ackData, sizeof(ackData)); // pre-load data
 }
 
-//==========
+//================
 void loop() {
-    getData();
+  getData();
+  if (newData == true) {
     showData();
+    cleanData();
+  }
 }
 
-//============
+//================
 void getData() {
     if ( radio.available() ) {
         radio.read( &dataReceived, sizeof(dataReceived) );
@@ -96,37 +95,17 @@ void getData() {
 }
 
 //================
-void showData() {
-    if (newData == true) {
-        Serial.print("Data received ");
-        Serial.println(dataReceived);
-        Serial.print(" ackPayload sent ");
-        Serial.print(ackData[0]);
-        Serial.print(", ");
-        Serial.println(ackData[1]);
-        newData = false;
-    }
-}
-
-//================
 void updateReplyData() {
   //radio.writeAckPayload(1, &ackData, sizeof(ackData)); // load the payload for the next time
-//     String message = "";
-//     message.concat(deviceID);
-//     message.concat(";");
-//     message.concat(handleDHT());
-//     message.concat(handleMHZ19());
-     // sends data in this format:
-//     // (float)(AM2120 - teplota); (float)(AM2120 - vlhost); (int)(MH-Z19 - UART - CO2); (int)(MH-Z19 - PWM - CO2); (int)(MH-Z19 - teplota);
-//     sendString(message);
-  Serial.println(sizeof(dataStruct));
-  radio.writeAckPayload(1, &dataStruct, sizeof(dataStruct));
+  handleDHT();
+  handleMHZ19();
 
+  radio.writeAckPayload(1, &dataStruct, sizeof(dataStruct));
 }
 
 // cte hodnoty z AM2120
-//==========
-String handleDHT() {
+//================
+void handleDHT() {
     // načtení teploty do proměnné
   float teplota = dht.readTemperature();
   // načtení vlhkosti do proměnné
@@ -134,53 +113,63 @@ String handleDHT() {
   // kontrola, jestli jsou načtené hodnoty čísla, pomocí funkce isnan
   if (isnan(teplota) || isnan(vlhkost)) {
     // při chybném čtení prazdne hodnoty
-    return ";;";
+    Serial.println("Err: handling DHT");
   } else {
-    String result = "";
-    result.concat(teplota);
-    result.concat(";");
-    result.concat(vlhkost);
-    result.concat(";");
-//    Serial.print(result);
-    return result;
+    dataStruct.teplota = teplota;
+    dataStruct.vlhkost = vlhkost;
+  //  Serial.println("Succ: DHT values refreshed");
   }
 }
 
 // cte hodnoty z MH-Z19
-//==========
-String handleMHZ19() {
-    // zahájení měření senzoru a načtení výsledků do proměnné
+//================
+void handleMHZ19() {
+  // zahájení měření senzoru a načtení výsledků do proměnné
+  
+  // Serial.println(mhz19_uart->getStatus());
+  if (mhz19_uart->getStatus() == 0) {
   measurement_t m = mhz19_uart->getMeasurement();
   int co2ppm = mhz19_pwm->getPpmPwm();
-  String result = "";
   // CO2 mereni
-  result.concat(map(m.co2_ppm, 0, 5000, 0, 2000));
-  result.concat(";");
+  dataStruct.co2ppm = map(m.co2_ppm, 0, 5000, 0, 2000);
   // CO2 mereni pres PWM
-  result.concat(co2ppm);
-  result.concat(";");
+  dataStruct.co2ppm2 = co2ppm;
   // teplota
-  result.concat(m.temperature);
-  result.concat(";");
-//  Serial.print(result);
-  return result;
+  dataStruct.teplota2 = m.temperature;
+  // Serial.println("Succ: MHZ19 values refreshed");
+  } else {
+    Serial.println("Err: handling MHZ19");
+  }
 }
 
-//==========
-void sendString(String message)
-{
-  byte messageLength = message.length() + 1;
+//================
+void showData() {
+        Serial.print("Data received ");
+        Serial.println(dataReceived);
+        printSensorData();
+}
 
-  // convert string to char array
-  char charBuffer[messageLength];
-  message.toCharArray(charBuffer, messageLength);
+//================
+void printSensorData() {
+  Serial.print(" Send data:");
+  Serial.print(dataStruct.teplota);
+  Serial.print("; vhlkost: ");
+  Serial.print(dataStruct.vlhkost);
+  Serial.print("; co2ppm: ");
+  Serial.print(dataStruct.co2ppm);
+  Serial.print("; co2ppm2: ");
+  Serial.print(dataStruct.co2ppm2);
+  Serial.print("; teplota2: ");
+  Serial.print(dataStruct.teplota2);
+  Serial.println();
+}
 
-  digitalWrite(13, true);
-  // Serial.print("sent: " + message);
-  Serial.println(message);
-  Serial.println(sizeof(ackData));
-  Serial.println(sizeof(message));
-
-  radio.writeAckPayload(1, &ackData, sizeof(ackData)); // load the payload for the next time
-  digitalWrite(13, false);
+//================
+void cleanData() {
+  dataStruct.teplota = 0.0f;
+  dataStruct.vlhkost = 0.0f;
+  dataStruct.co2ppm = 0;
+  dataStruct.co2ppm2 = 0;
+  dataStruct.teplota2 = 0;
+  newData = false;
 }
