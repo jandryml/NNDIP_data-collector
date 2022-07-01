3e0ec6fe2be12a83b0920308babf25b23c3ca4d8
Jan Dryml <honza.dryml@icloud.com>
2022-07-01 18:11:46 +0200

    docs - draft

--- data-collector/src/main/java/cz/edu/upce/fei/datacollector/service/communication/ModbusCommService.java
+++ data-collector/src/main/java/cz/edu/upce/fei/datacollector/service/communication/ModbusCommService.java
@@ -2,9 +2,20 @@ package cz.edu.upce.fei.datacollector.service.communication;
 
 import cz.edu.upce.fei.datacollector.model.Action;
 
+/**
+ * Handle communication to Modbus client.
+ */
 public interface ModbusCommService {
 
+    /**
+     * Writes value to coil of configured Modbus client.
+     * All necessary data (address and value) are derived from provided action param.
+     */
     void writeToCoil(Action action);
 
+    /**
+     * Writes value to register of configured Modbus client.
+     * All necessary data (address and value) are derived from provided action param.
+     */
     void writeToRegister(Action action);
 }
--- data-collector/src/main/java/cz/edu/upce/fei/datacollector/service/communication/RaspberryPiCommService.java
+++ data-collector/src/main/java/cz/edu/upce/fei/datacollector/service/communication/RaspberryPiCommService.java
@@ -3,10 +3,25 @@ package cz.edu.upce.fei.datacollector.service.communication;
 import cz.edu.upce.fei.datacollector.model.Action;
 import org.springframework.scheduling.annotation.Scheduled;
 
+/**
+ * Handle communication to RaspberryPi GPIO interface.
+ */
 public interface RaspberryPiCommService {
 
+    /**
+     * Writes value to GPIO pin.
+     * All necessary data (address and value) are derived from provided action param.
+     *
+     * Changes the GPIO pin to OUTPUT mode.
+     */
     void writeValue(Action action);
 
+    /**
+     * Removes all present GPIO listeners
+     * then fetches all active GPIO listeners and registers them.
+     *
+     * Changes the GPIO pin to INPUT mode.
+     */
     @Scheduled(cron = "${refreshGpioListenersPeriod}")
     void registerListeners();
 }
